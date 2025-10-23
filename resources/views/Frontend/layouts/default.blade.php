17dd26a1e6562820b71e2688f0eda33b04b1cb22
phuongtri1909 <tri2003bt@gmail.com>
2025-10-23 12:40:05 +0700

    update ads

--- resources/views/Frontend/layouts/default.blade.php
+++ resources/views/Frontend/layouts/default.blade.php
@@ -2,14 +2,14 @@
 <html lang="en">
 
 <head>
-    <script src="https://cmp.gatekeeperconsent.com/min.js" data-cfasync="false"></script>
-    <script src="https://the.gatekeeperconsent.com/cmp.min.js" data-cfasync="false"></script>
+    {{-- <script src="https://cmp.gatekeeperconsent.com/min.js" data-cfasync="false"></script>
+    <script src="https://the.gatekeeperconsent.com/cmp.min.js" data-cfasync="false"></script> --}}
 
-    <script async src="//www.ezojs.com/ezoic/sa.min.js"></script>
+    {{-- <script async src="//www.ezojs.com/ezoic/sa.min.js"></script>
     <script>
         window.ezstandalone = window.ezstandalone || {};
         ezstandalone.cmd = ezstandalone.cmd || [];
-    </script>
+    </script> --}}
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">

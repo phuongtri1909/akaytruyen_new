173a729ac1d271decd8e6a6e132d6c85705bc181
phuongtri1909 <tri2003bt@gmail.com>
2025-10-30 09:31:11 +0700

    update ads

--- resources/views/Frontend/components/adsense-scripts.blade.php
+++ resources/views/Frontend/components/adsense-scripts.blade.php
@@ -1,2 +1,13 @@
 <script async="async" data-cfasync="false"
         src="//acceptableremotehumorous.com/76289abe542de83208f592130273b5ee/invoke.js"></script>
+
+        <script type="text/javascript">
+          atOptions = {
+               'key' : '9859a1d2ae053d4bda564a392daa4379',
+               'format' : 'iframe',
+               'height' : 60,
+               'width' : 468,
+               'params' : {}
+          };
+     </script>
+     <script type="text/javascript" src="//acceptableremotehumorous.com/9859a1d2ae053d4bda564a392daa4379/invoke.js"></script>

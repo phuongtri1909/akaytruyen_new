7aa26473dda89e3329b35b21d879558aca8df25c
phuongtri1909 <tri2003bt@gmail.com>
2025-10-30 09:33:58 +0700

    update ads

--- resources/views/Frontend/components/adsense-scripts.blade.php
+++ resources/views/Frontend/components/adsense-scripts.blade.php
@@ -1,13 +1,14 @@
 <script async="async" data-cfasync="false"
-        src="//acceptableremotehumorous.com/76289abe542de83208f592130273b5ee/invoke.js"></script>
+    src="//acceptableremotehumorous.com/76289abe542de83208f592130273b5ee/invoke.js"></script>
 
-        <script type="text/javascript">
-          atOptions = {
-               'key' : '9859a1d2ae053d4bda564a392daa4379',
-               'format' : 'iframe',
-               'height' : 60,
-               'width' : 468,
-               'params' : {}
-          };
-     </script>
-     <script type="text/javascript" src="//acceptableremotehumorous.com/9859a1d2ae053d4bda564a392daa4379/invoke.js"></script>
+<script type="text/javascript">
+    atOptions = {
+        'key': '9859a1d2ae053d4bda564a392daa4379',
+        'format': 'iframe',
+        'height': 60,
+        'width': 468,
+        'params': {}
+    };
+</script>
+<script type="text/javascript" src="//acceptableremotehumorous.com/9859a1d2ae053d4bda564a392daa4379/invoke.js"></script>
+<script type='text/javascript' src='//acceptableremotehumorous.com/7d/59/e4/7d59e4f8f27662a5454001e69f6c8019.js'></script>

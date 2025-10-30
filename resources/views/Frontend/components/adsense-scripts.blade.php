0be4558b042e392c51c8c488c9f59acebf8d2ed1
phuongtri1909 <tri2003bt@gmail.com>
2025-10-30 13:42:57 +0700

    update ads

--- resources/views/Frontend/components/adsense-scripts.blade.php
+++ resources/views/Frontend/components/adsense-scripts.blade.php
@@ -1,5 +1,4 @@
-<script async="async" data-cfasync="false"
-    src="//acceptableremotehumorous.com/76289abe542de83208f592130273b5ee/invoke.js"></script>
+
 
 <script type="text/javascript">
     atOptions = {

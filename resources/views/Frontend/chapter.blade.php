8e64578ebcdf46493f404a399a0d5f18284778cd
phuongtri1909 <tri2003bt@gmail.com>
2025-10-31 16:54:39 +0700

    update ads

--- resources/views/Frontend/chapter.blade.php
+++ resources/views/Frontend/chapter.blade.php
@@ -243,8 +243,8 @@
             </script>
 
         </div>
-        
-        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-4405345005005059" data-ad-slot="4536454493"
+
+        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-4405345005005059" data-ad-slot="7544464049"
             data-ad-format="auto" data-full-width-responsive="true"></ins>
         <script>
             (adsbygoogle = window.adsbygoogle || []).push({});
